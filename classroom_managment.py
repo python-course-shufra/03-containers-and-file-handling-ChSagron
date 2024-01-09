261da07882441f54e02fe81cb93069c3eab7c512
ChSagron <ch4373@gmail.com>
2024-01-09 22:21:41 +0200

    complete

--- classroom_managment.py
+++ classroom_managment.py
@@ -42,31 +42,50 @@ def add_student(name, email=None):
              in lowercase, you can use the `s.lower()` method
     'grade': initialize with empty list
     """
-    pass
+    if (email is None):
+        email = f'{name}@example.com'
+    classroom.append({'name': name, 'email': email.lower(), 'grade': []})
+
+
+def find_student(name):
+    for i, s in enumerate(classroom):
+        if s['name'] == name:
+            return i
+    return -1
 
 
 def delete_student(name):
     """Delete a student from the classroom"""
-    pass
+    index = find_student(name)
+    del classroom[index]
 
 
 def set_email(name, email):
     """Sets the email of the student"""
-    pass
+    index = find_student(name)
+    classroom[index]['email'] = email.lower()
 
 
 def add_grade(name, profession, grade):
     """Adds a new grade to the student grades"""
-    pass
+    index = find_student(name)
+    classroom[index]['grades'].append((profession, grade))
 
 
 def avg_grade(name, profession):
     """Returns the average of grades of the student
     in the specified profession
     """
-    pass
+    sum, count = 0, 0
+    index = find_student(name)
+    for g in classroom[index]['grades']:
+        if g[0] == profession:
+            sum += g[1]
+            count += 1
+    return sum / count
 
 
 def get_professions(name):
     """Returns a list of unique professions that student has grades in"""
-    pass
+    index = find_student(name)
+    return list({pro for pro, _ in classroom[index]['grades']})
--- quiz_app.py
+++ quiz_app.py
@@ -14,7 +14,48 @@ def main():
     #       3.1. Parse the line (use s.split())
     #       3.2 Create a list of options
     #       3.3 random.shuffle(l)
-    pass
+
+    print(sys.argv)
+    file_name = sys.argv[1]
+    num = int(sys.argv[2])
+
+    listQ = convert_file_to_objects(file_name)
+
+    count = 0
+    for n in range(num):
+       count += print_question(listQ[n])
+
+    print(f'You answer {count}/{num} correct answers')
+
+
+# create question list
+def convert_file_to_objects(file_name):
+
+    listQ = []
+    with open(rf'questions/{file_name}') as file:
+        for line in file:
+            (question, answer, options) = line.split(';')
+            question = question.strip()
+            answer = answer.strip()
+            optionsList = options.split(',')
+            optionsList = [o.strip() for o in optionsList]
+            optionsList.append(answer)
+            random.shuffle(optionsList)
+            listQ.append({'question': question, 'answer': answer, 'optionsList': optionsList})
+
+    random.shuffle(listQ)
+    return listQ
+
+
+# print a question, return 1 if correct, else 0
+def print_question(q):
+    print(q['question'])
+    for i, o in enumerate(q['optionsList']):
+        print(f'{i+1}. {o}')
+    selected = int(input('Select the correct answer: '))
+    if q['optionsList'][selected-1] == q['answer']:
+        return 1
+    return 0
 
 
 if __name__ == '__main__':
